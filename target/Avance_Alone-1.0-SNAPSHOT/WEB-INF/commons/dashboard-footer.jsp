0a0173682ebf8ebc1ae7641aede4c45763ae949b
Juan <juandelaportillacardenas@gmail.com>
2023-05-03 13:10:25 -0500

    deleted unnecesary files, added gitignore

--- target/Avance_Alone-1.0-SNAPSHOT/WEB-INF/commons/dashboard-footer.jsp
+++
@@ -1,3 +0,0 @@
-<footer id="footer" class="footer">
-    <div class="copyright"> &copy; Copyright <strong>ALONE</strong>. Todos los Derechos Reservados</div>
-</footer>
--- target/Avance_Alone-1.0-SNAPSHOT/assets/js/slider-top.js
+++
@@ -1,9 +0,0 @@
-let swiper1 = new Swiper('.mySwiper', {
-    slidesPerView: 1,
-    spaceBetween: 0,
-    loop: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-});
